8c91e3445ddcecd3aa315486b5fa0fcff643072b
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-04-07 20:21:51 +0530

    disclaimer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Disclaimer! This website is currently saving data in local storage. Do not clear memory of chrome! API's are currenty in development! New Update coming Soooonnn..</h1>
       <LedgerTable />
     </div>
   );
