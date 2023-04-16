0dfc22c8ea5b3b6589c70664c3daf56d2df382b3
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-04-16 14:00:12 +0530

    added notice

--- src/components/ExpenseGraph.js
+++ src/components/ExpenseGraph.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ExpenseGraph() {
   return (
-    <div>ExpenseGraph</div>
+    <div className='expense-graph-screen' style={{display:"flex",justifyContent:"center"}}>Expense Graph Feature coming soon..</div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,12 @@ import "./Navbar.css";
 import { Link } from 'react-router-dom';
 function Navbar() {
   return (
-    <div className="navbar-screen" style={{alignItems:"center",backgroundColor:"black",color:"white",height:"5.5vh",display:"flex",gap:"10px",paddingLeft:"20px"}}>
-        <div className="nav-element"><Link to="/" style={{color:"white",textDecoration:"none"}}>Home</Link></div>
-        <div className="nav-element"><Link to="/expense-graph" style={{color:"white",textDecoration:"none"}}>Expense Graph</Link></div>
+    <div className="navbar-screen" style={{alignItems:"center",backgroundColor:"black",color:"white",height:"15vh",display:"flex",gap:"5px",justifyContent:"center",flexDirection:"column"}}>
+        <h2>SR LEDGER BOOK</h2>
+        <div className="na-elements" style={{display:"flex",gap:"10px",justifyContent:"center"}}>
+            <div className="nav-element"><Link to="/" style={{color:"white",textDecoration:"none"}}>Home</Link></div>
+            <div className="nav-element"><Link to="/expense-graph" style={{color:"white",textDecoration:"none"}}>Expense Graph</Link></div>
+        </div>
     </div>
   )
 }
